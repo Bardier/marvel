882f5efbb002095b390ce4d4c8f1356915de7006
Nick Bardier <bardier.pro@gmail.com>
2022-05-30 17:11:58 +0300

    Сделал загрузку RandomChar

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 export default class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
-  _apiKey = "apikey=cb4a48a9fb5b07c688d082e445e12e9c";
+  // _apiKey = "apikey=cb4a48a9fb5b07c688d082e445e12e9c"; // !bardier.pro@gmail.com
+  _apiKey = "apikey=e9a10b53cab1788f3ad0a31cfcbf0238"; // !byefar@i.ua
 
   getResource = async (url) => {
     const res = await fetch(url);
@@ -12,13 +13,30 @@ export default class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    return res.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : "There is no description for this character",
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }

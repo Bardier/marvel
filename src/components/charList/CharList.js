e3061989dc7aece3e51d4a7ca4e313229501de1d
Nick Bardier <bardier.pro@gmail.com>
2022-06-08 10:55:04 +0300

    ss

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -2,7 +2,7 @@ import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Component } from "react";
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"; // проверка props на тип данных
 
 import "./charList.scss";
 
